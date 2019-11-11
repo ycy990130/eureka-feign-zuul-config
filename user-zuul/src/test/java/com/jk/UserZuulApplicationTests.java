efb09ab02522773994c402f6826ba6fc80328cc3
ycy990130 <1835801084@qq.com>
2019-11-11 10:07:39 +0800

    bbb

---
+++ user-zuul/src/test/java/com/jk/UserZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
